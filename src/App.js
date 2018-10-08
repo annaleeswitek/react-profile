5748327be23040c2017acda3e534fecb38bb42b0
annaleeswitek <annaleejude@gmail.com>
2018-10-08 19:20:30 -0400

    removed unneeded styles and added headshot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import annalee from '../src/annalee.jpeg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to BLAH</h1>
+          <img src={annalee}  alt="Annalee Switek" className="headshot" />
+          <h1>Hi, I'm Annalee</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
